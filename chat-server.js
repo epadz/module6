419a7ae190a6aa3e25165ea9f32e24feeb23b932
epadz <e.padzensky@comcast.net>
2015-11-04 12:31:58 -0600

    work so far

--- chat-server.js
+++ chat-server.js
@@ -6,8 +6,19 @@ var http = require("http"),
 	mime = require('mime'),
 	fs = require("fs");
 
-var rooms = {};
-var users = {};
+var rooms = [{
+	name:'Open Forum',
+	pw:null,
+	admin: null,
+	users: []
+}];
+var users = [];
+var rid = 1;
+var uid = 0;
+var mid = 0;
+
+var dis = 0;
+var con = 0;
 
 var app = http.createServer(function(req, resp){
 	var filename = path.join(__dirname, "", url.parse(req.url).pathname);
@@ -50,29 +61,67 @@ app.listen(3456);
 var io = socketio.listen(app);
 io.sockets.on("connection", function(socket){
 	// This callback runs when a new Socket.IO connection is established.
- 
+	console.log("connect: " + con);
+	con++;
+	
+	socket.on('newUser', function(data){
+		usrIfo = {
+			un: data,
+			uid: users.length
+		};
+		users.push(usrIfo);
+		//console.log(users.toString());
+		
+		socket.emit('unResponse', usrIfo);
+		socket.un = data;
+		socket.uid = usrIfo.uid;
+		socket.info = usrIfo;
+		
+		rooms[0].users.push(usrIfo);
+		
+		io.sockets.emit('roomList', rooms);
+		socket.join("0");
+		socket.room = "0";
+		//console.log(JSON.stringify(rooms));
+	});
+	
+	
+	socket.on('newRoom', function(data){
+		rooms[rid] = data;
+		rid++;
+		//console.log(JSON.stringify(rooms));
+		io.sockets.emit('roomList', rooms);
+	});
+	
+	socket.on('enterRoom', function(data){
+		rooms[parseInt(socket.room)].users = rooms[parseInt(socket.room)].users.filter(function(val){
+			return val.uid != socket.uid;
+		});//removes user from rooms[room].users		
+		socket.leave(socket.room);
+		socket.join(data + "");
+		socket.room = data + "";
+		rooms[parseInt(socket.room)].users.push(socket.info);
+		socket.emit("enterRoomAlert",rooms);
+	});
+	
 	socket.on('message_to_server', function(data) {
 		// This callback runs when the server receives a new message from the client.
  		if(data["message"])
 		{
-			console.log("message: "+data["message"]); // log it to the Node.JS output
-			io.sockets.emit("message_to_client",{message:data["message"] }) // broadcast the message to other users
-		}else if(data["name"]){
-			console.log("roomName: "+data["name"]); // log it to the Node.JS output
-			io.sockets.emit("message_to_client",{name:data["name"] }) // broadcast the message to other users
+			//console.log("message: "+data["message"]); // log it to the Node.JS output
+			io.sockets.to(socket.room).emit("message_to_client",{poster: socket.un + "", message:data["message"] }) // broadcast the message to other users
 			
-			rooms.push({
-				name:data["name"] + "",
-				pw:data["pw"],
-				admin: data["user"] + "",
-				users: [data["user"] + ""]
-			});
-			console.log(JSON.stringify(rooms));
-		}else if(data["userName"]){
-			console.log("userName: "+ data["userName"]); // log it to the Node.JS output
-			//needs to check if user exists
-			users[users.length] = data["userName"] + "";			
-		}
-		
+		}		
 	});
+	
+	socket.on('disconnect', function(){
+		console.log("disconnect: " + dis);
+		dis++;
+		rooms[parseInt(socket.room)].users = rooms[parseInt(socket.room)].users.filter(function(val){
+			return val.uid != socket.uid;
+		});//removes user from rooms[room].users		
+		socket.leave(socket.room);
+		socket.emit("enterRoomAlert",rooms);
+	});
+	
 });
